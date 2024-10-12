86881761891ff82229e161c9645a7a9795ea5fb3
JoshDDutton <51129846+JoshDDutton@users.noreply.github.com>
2024-10-11 19:03:35 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.getElementById('imageModal');
+    var img = document.getElementById('portrait');
+    var modalImg = document.getElementById('modalImage');
+    var closeBtn = document.getElementsByClassName('close')[0];
+
+    img.onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        document.body.classList.add('modal-open');
+    }
+
+    closeBtn.onclick = closeModal;
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            closeModal();
+        }
+    }
+
+    function closeModal() {
+        modal.style.display = "none";
+        document.body.classList.remove('modal-open');
+    }
+});
 
