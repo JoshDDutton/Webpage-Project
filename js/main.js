d42b5eec2cf0fab4ce7cfe4ef9d070b9c829a432
JoshDDutton <51129846+JoshDDutton@users.noreply.github.com>
2024-10-11 19:19:04 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,11 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var img = document.getElementById('portrait');
     var modalImg = document.getElementById('modalImage');
     var closeBtn = document.getElementsByClassName('close')[0];
+    var contentWrapper = document.querySelector('.content-wrapper');
 
     img.onclick = function() {
         modal.style.display = "block";
         modalImg.src = this.src;
         document.body.classList.add('modal-open');
+        contentWrapper.style.filter = 'blur(5px)';
+        modal.style.filter = 'none'; // Ensure modal is not blurred
     }
 
     closeBtn.onclick = closeModal;
@@ -21,6 +24,6 @@ document.addEventListener('DOMContentLoaded', function() {
     function closeModal() {
         modal.style.display = "none";
         document.body.classList.remove('modal-open');
+        contentWrapper.style.filter = 'none';
     }
 });
-
